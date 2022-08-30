242b570e6ecb31aa6900f14847dfbbae003e4235
rissuga <risairma21@gmail.com>
2022-08-30 12:43:34 +0700

    ubah nama

--- proses/create_barang.php
+++ proses/create_barang.php
@@ -6,13 +6,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $nomor = $_POST['nomor'];
   $nama = $_POST['nama'];
   $warna = $_POST['warna'];
-  $jumlah = $_POST['jumlah'];
+  $jumlah = $_POST['umlah'];
 
 
 
   $query = "INSERT INTO barang SET nomor= '$nomor', nama= '$nama' , warna = '$warna' , jumlah = '$jumlah' ";
 
-  mysqli_query($conn, $query);
+  mysqli_query(
+    $conn,
+    $query
+  );
   header('location:../listbarang.php');
 
   mysqli_close($conn);
--- utils/koneksi.php
+++ utils/koneksi.php
@@ -13,6 +13,5 @@ $conn = mysqli_connect($servername, $username, $password, "$dbname");
 if (!$conn) {
   die("Koneksi gagal: " . mysqli_connect_error());
 } else {
-
   echo "Koneksi berhasil";
 }
